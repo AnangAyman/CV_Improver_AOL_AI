9b512b85ceb1972b51654480a1d98190f90e5ef1
Ayman <anang.ayman@gmail.com>
2024-12-16 18:39:18 +0700

    Combined backend and frontend for Score bar

--- AI.py
+++ AI.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-# from heuristics import heuristic_score
+
 
 model_name = 'gpt2-medium'  # You can use 'gpt2-medium' for a larger model
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
